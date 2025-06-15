d8e3a902951c398fbcbf4c7579184e0391f168ae
Noah Mandelbaum <nmande@comcast.net>
2025-06-15 08:43:50 -0400

    Added more nuanced buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,33 @@
-import { StyleSheet, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from '@/components/Button';
-import ImageViewer from '@/components/ImageViewer'; 
+import ImageViewer from '@/components/ImageViewer';
 
-const PlaceholderImage = require('@/assets/images/soft-pta.png');
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
       </View>
-       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
     </View>
@@ -28,7 +43,7 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
-    footerContainer: {
+  footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
   },
